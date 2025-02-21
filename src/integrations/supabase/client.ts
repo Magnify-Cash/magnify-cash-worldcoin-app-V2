f097317bbea8805c3ee2af79b7b79cc49778d840
DanielHighETH <danielhigheth@gmail.com>
2025-02-21 13:32:02 +0800

    Added headers to supabase request

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -5,4 +5,12 @@ import type { Database } from '@/types/supabase/database';
 const SUPABASE_URL = "https://npqctynrdzopspxhtpwa.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wcWN0eW5yZHpvcHNweGh0cHdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzcwMTYzODAsImV4cCI6MjA1MjU5MjM4MH0.R9q4-yIPcI7zm9YJQcfNuOEE7z7BF_nt9d2DXKWlBb8";
 
-export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
+    global: {
+      headers: {
+        "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0",
+        "Pragma": "no-cache",
+        "Expires": "0",
+      },
+    },
+  });

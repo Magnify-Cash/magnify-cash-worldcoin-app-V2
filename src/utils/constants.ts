4f69b20a93a74f68c46322477a3ef9991675c8ab
DanielHighETH <danielhigheth@gmail.com>
2025-03-10 20:00:35 +0800

    added developer/staging contract address

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 export const WORLDCOIN_TOKEN_COLLATERAL = "0x79A02482A880bCE3F13e09Da970dC34db4CD24d1";
 export const WORLDCOIN_CLIENT_ID = import.meta.env.VITE_WORLDCOIN_CLIENT_ID;
-export const MAGNIFY_WORLD_ADDRESS = "0x93dbB2d447F0086aF60B2becc66598fe3D9135A1";
+export const MAGNIFY_WORLD_ADDRESS = import.meta.env.VITE_MAGNIFY_WORLD_ADDRESS;
 export const MAGNIFY_WORLD_ADDRESS_V1 = "0x4E52d9e8d2F70aD1805084BA4fa849dC991E7c88";
 export const WORLDCHAIN_RPC_URL = "https://worldchain-mainnet.g.alchemy.com/public"
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;

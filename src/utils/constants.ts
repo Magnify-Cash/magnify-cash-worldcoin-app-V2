91b63740f4713aa32f80da93927674f8de6e6758
DanielHighETH <danielhigheth@gmail.com>
2025-03-12 00:14:56 +0800

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const WORLDCOIN_TOKEN_COLLATERAL = "0x79A02482A880bCE3F13e09Da970dC34db4CD24d1";
-export const WORLDCOIN_CLIENT_ID = import.meta.env.VITE_WORLDCOIN_CLIENT_ID;
+export const WORLDCOIN_CLIENT_ID = "app_9b99a6a671d5a3de5ad369964a0ac4d7";
 export const MAGNIFY_WORLD_ADDRESS = "0x93dbB2d447F0086aF60B2becc66598fe3D9135A1";
 export const MAGNIFY_WORLD_ADDRESS_V1 = "0x4E52d9e8d2F70aD1805084BA4fa849dC991E7c88";
 export const WORLDCHAIN_RPC_URL = "https://worldchain-mainnet.g.alchemy.com/public"

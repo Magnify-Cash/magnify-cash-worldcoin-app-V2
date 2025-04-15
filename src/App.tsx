23f1703eaf834ef64c32544635d312623e0c1668
DanielHighETH <danielhigheth@gmail.com>
2025-04-15 18:44:36 +0200

    Revert "Update App.tsx"
    This reverts commit 91d1c8991373918d1e58ddd8c23a7d3d0de50e0b.

--- src/App.tsx
+++ src/App.tsx
@@ -21,8 +21,7 @@ const allowedWallets = [
   "0x2f79325b76cd2109cd9cf5320b6d23d7f682d65c",
   "0x7af5e0de231d82def3bc262b1d5b3359495a4bfb",
   "0xf0c7db5acea62029058b0e4e0b79f2bac18686c4",
-  "0x6a7ec268afb31dab2b0ad39511af9db7c11944a1",
-  "0x65bc1d86e75e7c8dd46d53d6286ff67372cb1f59"
+  "0x6a7ec268afb31dab2b0ad39511af9db7c11944a1"
 ];
 
 // Hard-coded to developer's wallet address
